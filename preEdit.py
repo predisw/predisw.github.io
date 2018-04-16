d3885a4a9ca316389dc1b49aab228b357e38e3a0
predisw <predisw@sina.com>
2018-04-17 00:10:18 +0800

    add preEdit.py

--- preEdit.py
+++ preEdit.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
 from datetime import datetime, timedelta
 import commands
+import os
 
+"""
+version: 1.0.0
+date: 2018-04-17
+
+"""
 
 TITLE = "title"
 CATEGORIES= "categories"
 TAGS = "tags"
-ORIGIN_SOURCE = "source/_source"
+ORIGIN_SOURCE = "source/_sources"
 BUILD_SOURCE = "source/_posts"
 
-return_code,path = commands.getstatusoutput("pwd");
-print "build path is "+path
+return_code,basePath = commands.getstatusoutput("pwd");
+print "build path is "+basePath
 
-git_cmd = 'git log -1 --pretty="%B"';
+get_last_commit_msg= 'git log -1 --pretty="%B"';
 
 
 def getBuildDatetime():
@@ -24,10 +30,10 @@ def getBuildDatetime():
 
 
 def getCommitMsgList():
-    return_code,output = commands.getstatusoutput(git_cmd);
+    return_code,output = commands.getstatusoutput(get_last_commit_msg);
     if return_code == 0:
         if TITLE in output:
-            return output.split()
+            return output.split("\n")
     else:
         raise Exception("can't get commit message",output)
 
@@ -37,12 +43,13 @@ def commitMsgToMap(msg):
     heads = dict()
     infos = msg.split(";")
     for info in infos:
-        heads[info.split(":")[0]] = info.split(":")[1]
+        if info:
+            heads[info.split(":")[0]] = info.split(":")[1]
     print "heads of blog is {}".format(heads)
     return heads
 
 def writeMsgToFile(msgMap):
-    fileName = "{0}/{1}/{2}.md".format(path,BUILD_SOURCE,msgMap[TITLE])
+    fileName = "{0}/{1}/{2}.md".format(basePath,BUILD_SOURCE,msgMap[TITLE])
     print "fileName is {}".format(fileName)
     with open(fileName,"w") as f:
         headStr = concatBlogHeadFromCommitMsg(msgMap)
@@ -70,16 +77,26 @@ def concatBlogHeadFromCommitMsg(msgMap):
     return headStr
 
 
+def mergeHeadBodyToBlog(title):
+    source_file = os.path.join(basePath,ORIGIN_SOURCE,title+".md")
+    posts_file = os.path.join(basePath,BUILD_SOURCE,title+".md")
+    with open(posts_file,"a") as f:
+        for line in open(source_file,"r").readlines():
+            f.write(line)
 
 
-def main():
+
+
+def addHeadToBlogFromCommitMessage():
     msgsList = getCommitMsgList()
     if msgsList:
         for msg in msgsList:
-            msgHeads = commitMsgToMap(msg)
-            writeMsgToFile(msgHeads)
+            if msg:
+                msgHeads = commitMsgToMap(msg)
+                writeMsgToFile(msgHeads)
+                mergeHeadBodyToBlog(msgHeads[TITLE])
 
 
 
 if  __name__ == '__main__':
-    main()
+    addHeadToBlogFromCommitMessage()
