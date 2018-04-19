bd1c254b3235d7fdd21fea961f863dd4c5660b69
predisw <predisw@sina.com>
2018-04-19 22:33:17 +0800

    add pushToGit.sh

--- pushToGit.sh
+++ pushToGit.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if [ "$IS_PUSH" == "yes" ];then
+last_commit_msg=`git log -1 --pretty="%B"`;
+echo "$last_commit_msg";
+
+is_push=$(echo "$last_commit_msg" |grep "title")
+echo "$is_push";
+
+if [ "$is_push" != "" ];then
   hexo clean;
   git add .;
   git commit -m "push to source either after edit";
